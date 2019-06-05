f6fe8a2c0c0c0eebbd19ead5f8c15c35a5747b27
reiko watanabe <blackgothy@gmail.com>
2019-06-05 15:27:16 +0900

    投稿機能の基本設定

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,70 @@
 class PicturesController < ApplicationController
+
+    before_action :set_picture, only: [:show, :edit, :update, :destroy]
+
+    before_action :sign_in_user
+
+    def index
+      @pictures = Picture.all
+    end
+
+    def new
+      if params[:back]
+        @picture = Picture.new(picture_params)
+      else
+        @picture = Picture.new
+      end
+    end
+
+    def create
+      @picture = Picture.new(picture_params)
+      @picture.user_id = current_user.id
+      if @picture.save
+        redirect_to pictures_path, notice: "ブログを作成しました!"
+      else
+        render 'new'
+      end
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+      if @picture.update(picture_params)
+        redirect_to pictures_path, notice: "ブログを編集しました！"
+      else
+        render 'edit'
+      end
+    end
+
+    def destroy
+      @picture.destroy
+      redirect_to pictures_path, notice: "ブログを削除しました！"
+    end
+
+
+    def confirm
+      @picture = Picture.new(picture_params)
+      @picture.user_id = current_user.id
+      render :new if @picture.invalid?
+    end
+
+    private
+
+    def picture_params
+      params.require(:picture).permit(:content, :user_id, :image, :image_cache)
+    end
+
+    def set_picture
+      @picture = Picture.find(params[:id])
+    end
+
+    def sign_in_user
+      redirect_to new_user_path unless logged_in?
+    end
+
+
 end
