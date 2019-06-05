a988574adc15fac060e21cb6e0ee05299a621f97
reiko watanabe <blackgothy@gmail.com>
2019-06-05 21:16:30 +0900

    バグ修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:name, :email, :password, :pasword_confirmation, :avatar)
+        params.require(:user).permit(:name, :email, :password, :pasword_confirmation, :avatar, :avatar_cache)
     end
 end
