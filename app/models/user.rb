cd64b6509ca66d90ae75051e309df2ebe3cae1d5
reiko watanabe <blackgothy@gmail.com>
2019-06-07 08:46:56 +0900

    画像アップローダ追加インストール

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,6 @@ class User < ApplicationRecord
     has_many :pictures
     has_many :favorites, dependent: :destroy
 
-    mount_uploader :avatar, ImageUploader
+    mount_uploader :avatar, AvatarUploader
 
 end
